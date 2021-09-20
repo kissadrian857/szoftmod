c513c454b097abcf92fe82b8933354b5d1dbe4bd
Kiss Adrián Imre <kisswadrian857@gmail.com>
2021-09-20 15:17:31 +0200

    Initial commit

---
+++ src/main/java/hu/unideb/notetakingapp/NoteTakingAppApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.notetakingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteTakingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteTakingAppApplication.class, args);
+    }
+
+}
