a80b7acb11d231979b6eca80e86949624a39ffe5
Kiss Adrián Imre <kisswadrian857@gmail.com>
2021-09-21 19:11:01 +0200

    Added bootstrap package and BootStrapData.java for testing persistence

---
+++ src/main/java/hu/unideb/notetakingapp/bootstrap/BootStrapData.java
@@ -0,0 +1,43 @@
+package hu.unideb.notetakingapp.bootstrap;
+
+import hu.unideb.notetakingapp.api.entity.Note;
+import hu.unideb.notetakingapp.api.entity.User;
+import hu.unideb.notetakingapp.backend.dao.NoteRepository;
+import hu.unideb.notetakingapp.backend.dao.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final NoteRepository noteRepository;
+
+    private final UserRepository userRepository;
+
+    //itt injektáljuk be a bean-eket!
+    public BootStrapData(NoteRepository noteRepository, UserRepository userRepository) {
+        this.noteRepository = noteRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user1 = new User("randomUserName", "randomPasswd");
+        User user2 = new User("randomUserName2", "randomPasswd2");
+        Note note = new Note("randomBody...", LocalDate.now(), null, user1);
+        Note note2 = new Note("randomBody2...", LocalDate.now(), null, user1);
+        Note note3 = new Note("randomBody3...", LocalDate.now(), null, user2);
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+
+        noteRepository.save(note);
+        noteRepository.save(note2);
+        noteRepository.save(note3);
+
+        System.out.println("Started in Bootstrap...");
+        System.out.println("Number of notes: " + noteRepository.count());
+    }
+}
