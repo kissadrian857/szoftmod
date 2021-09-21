231d4d2148c9ffad6bfb26bc9935f7eef420879a
Kiss Adrián Imre <kisswadrian857@gmail.com>
2021-09-21 20:22:44 +0200

    spring.h2.console.enabled property set to true in application.properties

--- src/main/java/hu/unideb/notetakingapp/bootstrap/BootStrapData.java
+++ src/main/java/hu/unideb/notetakingapp/bootstrap/BootStrapData.java
@@ -39,5 +39,6 @@ public class BootStrapData implements CommandLineRunner {
 
         System.out.println("Started in Bootstrap...");
         System.out.println("Number of notes: " + noteRepository.count());
+        System.out.println("Number of users:" + userRepository.count());
     }
 }
