8a293435a145011ed1e2bf9b6f630bdf164a7acd
KristofKiss <kristofkiss3@gmail.com>
2021-11-23 23:44:24 +0100

    User can purchase to view a note

--- src/main/java/hu/unideb/notetakingapp/frontend/controller/BrowseController.java
+++ src/main/java/hu/unideb/notetakingapp/frontend/controller/BrowseController.java
@@ -1,22 +1,34 @@
 package hu.unideb.notetakingapp.frontend.controller;
 
+import hu.unideb.notetakingapp.api.entity.Note;
+import hu.unideb.notetakingapp.api.entity.Purchase;
+import hu.unideb.notetakingapp.api.entity.User;
 import hu.unideb.notetakingapp.api.service.NoteService;
+import hu.unideb.notetakingapp.api.service.PurchaseService;
 import hu.unideb.notetakingapp.api.service.UserService;
 import hu.unideb.notetakingapp.frontend.controller.helper.LoggedInUserBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 public class BrowseController {
     private final LoggedInUserBean loggedInUserBean;
     private final UserService userService;
     private final NoteService noteService;
+    private final PurchaseService purchaseService;
 
-    public BrowseController(LoggedInUserBean loggedInUserBean, UserService userService, NoteService noteService) {
+    public BrowseController(LoggedInUserBean loggedInUserBean, UserService userService, NoteService noteService, PurchaseService purchaseService) {
         this.loggedInUserBean = loggedInUserBean;
         this.userService = userService;
         this.noteService = noteService;
+        this.purchaseService = purchaseService;
     }
 
     @GetMapping({"/browse"})
@@ -24,11 +36,38 @@ public class BrowseController {
         model.addAttribute("loggedInUser", loggedInUserBean.getLoggedInUser());
 
         if (loggedInUserBean.isLoggedIn()) {
-            model.addAttribute("all_notes", noteService.findNotesExceptId(loggedInUserBean.getLoggedInUser().getId()));
+            List<Long> purchases = purchaseService.getPurchasedNotesById(loggedInUserBean.getLoggedInUser().getId());
+            List<Note> notes = noteService.findNotesExceptId(loggedInUserBean.getLoggedInUser().getId());
+
+            for (Note note : notes) {
+                if (!purchases.contains(note.getId()))
+                    note.setBody("");
+            }
+            model.addAttribute("all_notes", notes);
         } else {
             model.addAttribute("all_notes", noteService.getFreeNotes());
         }
 
         return "browse";
+    }
+
+    @GetMapping({"/buy"})
+    public String purchase(@RequestParam Long id,Model model){
+        if (!loggedInUserBean.isLoggedIn())
+            return "redirect:/login";
+
+        Note actNote = noteService.findById(id);
+        User actUser = loggedInUserBean.getLoggedInUser();
+        if(actUser.getCredits()>= actNote.getCreditValue()){
+            Purchase newPurchase = new Purchase();
+            newPurchase.setPurchaseTime(LocalDateTime.now());
+            newPurchase.setCustomerId(actUser.getId());
+            newPurchase.setNote(actNote);
+            newPurchase.setCreatorId(actNote.getCreatorUser().getId());
+            actUser.setCredits(actUser.getCredits()-actNote.getCreditValue());
+            purchaseService.save(newPurchase);
+            userService.save(actUser);
+        }
+        return "redirect:/browse";
     }
 }
